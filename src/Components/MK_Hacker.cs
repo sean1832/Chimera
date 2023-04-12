7b613babb48b2501598662e5472ac744dfa8f450
sean1832 <sean1832725142@gmail.com>
2023-04-13 02:26:17 +1000

    feat: added modular component button

--- src/Components/MK_Hacker.cs
+++ src/Components/MK_Hacker.cs
@@ -8,65 +8,19 @@ using Grasshopper.GUI;
 using Grasshopper.GUI.Canvas;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Special;
+using Monkey.src.UI;
 using Rhino.Geometry;
 
 
 namespace Monkey.src.Components
 {
-    public class Attributes_Custom : Grasshopper.Kernel.Attributes.GH_ComponentAttributes
-    {
-        public Attributes_Custom(GH_Component owner) : base(owner) { }
-
-        protected override void Layout()
-        {
-            base.Layout();
-
-            System.Drawing.Rectangle rec0 = GH_Convert.ToRectangle(Bounds);
-            rec0.Height += 22;
-
-            System.Drawing.Rectangle rec1 = rec0;
-            rec1.Y = rec1.Bottom - 22;
-            rec1.Height = 22;
-            rec1.Inflate(-2, -2);
-
-            Bounds = rec0;
-            ButtonBounds = rec1;
-        }
-        private System.Drawing.Rectangle ButtonBounds { get; set; }
-
-        protected override void Render(GH_Canvas canvas, System.Drawing.Graphics graphics, GH_CanvasChannel channel)
-        {
-            base.Render(canvas, graphics, channel);
-
-            if (channel == GH_CanvasChannel.Objects)
-            {
-                GH_Capsule button = GH_Capsule.CreateTextCapsule(ButtonBounds, ButtonBounds, GH_Palette.Black, "Hack", 2, 0);
-                button.Render(graphics, Selected, Owner.Locked, false);
-                button.Dispose();
-            }
-        }
-        public override GH_ObjectResponse RespondToMouseDown(GH_Canvas sender, GH_CanvasMouseEvent e)
-        {
-            if (e.Button == System.Windows.Forms.MouseButtons.Left)
-            {
-                System.Drawing.RectangleF rec = ButtonBounds;
-                if (rec.Contains(e.CanvasLocation))
-                {
-                    (Owner as MK_Hacker).TriggerHack();
-                    return GH_ObjectResponse.Handled;
-                }
-            }
-            return base.RespondToMouseDown(sender, e);
-        }
-    }
-
     public class MK_Hacker : GH_Component
     {
         private GH_Document GrasshopperDocument;
         private IGH_Component Component;
         private bool hack;
 
-        public void TriggerHack()
+        public void Hack()
         {
             hack = true;
             ExpireSolution(true);
@@ -210,7 +164,7 @@ namespace Monkey.src.Components
 
         public override void CreateAttributes()
         {
-            m_attributes = new Attributes_Custom(this);
+            m_attributes = new ComponentButton(this, "Hack", Hack);
         }
         /// <summary>
         /// Gets the unique ID for this component. Do not change this ID after release.
