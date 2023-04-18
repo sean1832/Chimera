d874847a2f122a089e3dea159fa173281b221291
sean1832 <sean1832725142@gmail.com>
2023-04-19 03:28:50 +1000

    fix: icon resource did not update

--- src/Components/Decipher.cs
+++ src/Components/Decipher.cs
@@ -134,7 +134,7 @@ namespace Monkey.src.Components
             {
                 //You can add image files to your project resources and access them like this:
                 // return Resources.IconForThisComponent;
-                return Properties.Resources.Monkey___Decipher;
+                return Properties.Resources.Decipher;
             }
         }
 
