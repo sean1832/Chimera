c526c5072e8feab7db5432f85ef84e8921bbc2c3
sean1832 <sean1832725142@gmail.com>
2023-04-21 21:11:12 +1000

    feat: add component Assembly Info to retrieve dependency information of a grasshopper script

---
+++ src/Components/AssemblyInfos.cs
@@ -0,0 +1,108 @@
+﻿using Grasshopper.Kernel;
+using Monkey.src.UI;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+
+namespace Monkey.src.Components
+{
+    public class AssemblyInfos : GH_Component
+    {
+        #region Metadata
+
+        public AssemblyInfos()
+          : base("Dependency", "Depends",
+              "Checks and outputs information about the plugins used in the current Grasshopper file, their versions, and the assembly info of all plugins available on this computer.",
+              "Monkey", "Utility")
+        {
+        }
+        public override GH_Exposure Exposure => GH_Exposure.secondary;
+        public override IEnumerable<string> Keywords => new string[] { "dependency", "requirements", "assembly", "assemblyinfo", "plugins" };
+        protected override System.Drawing.Bitmap Icon => null; // Properties.Resources.Icon
+        public override Guid ComponentGuid => new Guid("2f210d7b-c155-4598-98b6-b4c557224175");
+
+        #endregion
+
+        #region IO
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("PluginDependency", "D", "Checks dependency of current Grasshopper file.", GH_ParamAccess.list);
+            pManager.AddTextParameter("AssemblyInfo", "A", "Assembly info of all plugins available on this computer.", GH_ParamAccess.list);
+        }
+
+        #endregion
+
+        #region Button
+
+        public override void CreateAttributes()
+        {
+            m_attributes = new ComponentButton(this, "Refresh", Refresh);
+        }
+        private void Refresh()
+        {
+            ExpireSolution(true);
+        }
+
+        #endregion
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var dependency = ConvertDictToList(GetDependency());
+            var assembly = ConvertDictToList(GetAllAssembly());
+
+            DA.SetDataList(0, dependency);
+            DA.SetDataList(1, assembly);
+        }
+
+        #region Additional
+
+        private Dictionary<string, string> GetAllAssembly()
+        {
+            Dictionary<string, string> assemblyDict = new Dictionary<string, string>();
+
+            foreach (var obj in Grasshopper.Instances.ComponentServer.ObjectProxies)
+            {
+                var assembly = Grasshopper.Instances.ComponentServer.FindAssemblyByObject(obj.Guid);
+
+                if (assembly != null && !assembly.IsCoreLibrary)
+                {
+                    assemblyDict[assembly.Name] = assembly.Version;
+                }
+            }
+            return assemblyDict;
+        }
+
+        private Dictionary<string, string> GetDependency()
+        {
+            Dictionary<string, string> dependencyDict = new Dictionary<string, string>();
+
+            foreach (var obj in Grasshopper.Instances.ActiveCanvas.Document.Objects)
+            {
+                GH_AssemblyInfo assembly = Grasshopper.Instances.ComponentServer.FindAssemblyByObject(obj.ComponentGuid);
+                if (assembly != null && !assembly.IsCoreLibrary)
+                {
+                    dependencyDict[assembly.Name] = assembly.Version;
+                }
+            }
+            return dependencyDict;
+        }
+
+        private List<string> ConvertDictToList(Dictionary<string, string> dict)
+        {
+            List<string> list = new List<string>();
+            foreach (var item in dict)
+            {
+                list.Add(item.Key + " : " + item.Value);
+            }
+            return list;
+        }
+
+        #endregion
+    }
+}
